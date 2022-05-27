6fe287d8b222e7261c97483f3d9c9941d8b571ff
Rob Meijerink <rmeijerink@avancecommunicatie.nl>
2022-05-27 10:50:36 +0200

    autocomplt path

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -41,6 +41,7 @@ return require('packer').startup(function(use)
     use {'neovim/nvim-lspconfig', config = "require('lsp')"}
     use {'hrsh7th/cmp-nvim-lsp'}
     use {'hrsh7th/cmp-buffer'}
+    use {'hrsh7th/cmp-path'}
     use {'hrsh7th/nvim-cmp'}
     use {'hrsh7th/cmp-vsnip'}
     use {'kdheepak/lazygit.nvim', config = "require('lazygit-config')"}
