bcfb3509396091d3db7932b287718298b60334a8
Rob Meijerink <rmeijerink@avancecommunicatie.nl>
2022-06-21 09:21:52 +0200

    intelephense

--- .config/nvim/lua/lsp/intelephense_conf.lua
+++ .config/nvim/lua/lsp/intelephense_conf.lua
@@ -1,5 +1,7 @@
 require('lspconfig').intelephense.setup({
     on_attach = function(client, bufnr)
+      local opts = { noremap = true, silent = true }
+      
       -- Enable (omnifunc) completion triggered by <c-x><c-o>
       vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
       vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
