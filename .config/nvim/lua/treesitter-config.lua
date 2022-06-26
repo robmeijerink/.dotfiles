f48561e7bd7ac28f9ac1df544c1c6a3f74ec9379
Rob Meijerink <rmeijerink@avancecommunicatie.nl>
2022-06-27 00:45:38 +0200

    indents

--- .config/nvim/lua/treesitter-config.lua
+++ .config/nvim/lua/treesitter-config.lua
@@ -30,7 +30,7 @@ require'nvim-treesitter.configs'.setup {
     }
   },
   indent = {
-    enable = false -- Really breaks stuff if true
+    enable = true
   },
   incremental_selection = {
     enable = true,
