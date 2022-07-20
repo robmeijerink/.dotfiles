c1d88994b18ea8f70432cc1662d9c6ce312e788b
Rob <rmeijerink@avancecommunicatie.nl>
2022-07-20 10:11:23 +0200

    fix windows terminal issue bold+italics

--- .config/nvim/lua/bufferline-config.lua
+++ .config/nvim/lua/bufferline-config.lua
@@ -1,5 +1,10 @@
 require('bufferline').setup {
     options = {
         offsets = {{filetype = "NvimTree", text = "" , text_align = "left" }},
+    },
+    highlights = {
+        buffer_selected = {
+            gui = "italic"
+        },
     }
 }
