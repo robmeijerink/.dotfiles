554e84421e534e599e8d93fdbe92e7ef21334eac
Rob <rmeijerink@avancecommunicatie.nl>
2022-08-15 15:29:40 +0200

    html lsp disabled

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -5,7 +5,7 @@ require('lsp/language_servers')
 -- Server config
 require('lsp/server-conf/emmet-ls_conf')
 require('lsp/server-conf/intelephense_conf')
-require('lsp/server-conf/html_conf')
+-- require('lsp/server-conf/html_conf')
 require('lsp/server-conf/tailwindcss_conf')
 require('lsp/server-conf/ts_conf')
 require('lsp/server-conf/volar_conf')
