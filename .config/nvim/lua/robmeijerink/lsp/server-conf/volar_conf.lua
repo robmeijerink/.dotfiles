ca88cb6e1fa8a7649662fd4318a4d562b3b93d51
Rob <rmeijerink@avancecommunicatie.nl>
2022-09-08 16:06:46 +0200

    vue no formatting

--- .config/nvim/lua/robmeijerink/lsp/server-conf/volar_conf.lua
+++ .config/nvim/lua/robmeijerink/lsp/server-conf/volar_conf.lua
@@ -10,8 +10,8 @@ require('lspconfig').volar.setup{
   },
   on_attach = function(client, bufnr)
     -- Only use null-ls for formatting
-    -- client.resolved_capabilities.document_formatting = false
-    -- client.resolved_capabilities.document_range_formatting = false
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
 
     custom_attach(client, bufnr)
   end
