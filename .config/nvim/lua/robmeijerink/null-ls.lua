13e01cd3e1059d255a44a6248824058c87a2a8ed
Rob <rmeijerink@avancecommunicatie.nl>
2022-09-09 16:48:37 +0200

    fixes php formatter

--- .config/nvim/lua/robmeijerink/null-ls.lua
+++ .config/nvim/lua/robmeijerink/null-ls.lua
@@ -8,24 +8,34 @@ null_ls.setup({
   -- debug = true,
   debounce = 150,
   root_dir = utils.root_pattern("composer.json", "package.json", "Makefile", ".git"), -- Add composer
-  diagnostics_format = "#{m} (#{c}) [#{s}]",    -- Makes PHPCS errors more readeable
+  diagnostics_format = "#{m} (#{c}) [#{s}]", -- Makes PHPCS errors more readeable
   sources = {
-    null_ls.builtins.completion.spell,        -- You still need to execute `:set spell`
+    null_ls.builtins.completion.spell, -- You still need to execute `:set spell`
     -- null_ls.builtins.diagnostics.eslint,      -- Add eslint to js projects
     null_ls.builtins.diagnostics.phpcs.with({ -- Change how the php linting will work
-       prefer_local = "vendor/bin",
+      prefer_local = "vendor/bin",
+      extra_args = {
+        "--ignore=vendor",
+        "--no-interaction",
+        "--quiet"
+      },
     }),
     -- null_ls.builtins.formatting.stylua,       -- You need to install stylua first: `brew install stylua`
     null_ls.builtins.formatting.phpcbf.with({ -- Use the local installation first
-        prefer_local = "vendor/bin",
+      prefer_local = "vendor/bin",
+      extra_args = {
+        "--ignore=vendor",
+        "--no-interaction",
+        "--quiet"
+      },
     }),
     formatting.blade_formatter,
     formatting.prettier.with({
-        extra_args = {
-          "--no-semi",
-          "--single-quote",
-          "--jsx-single-quote",
-        }
+      extra_args = {
+        "--no-semi",
+        "--single-quote",
+        "--jsx-single-quote",
+      }
     }),
     formatting.black,
     formatting.gofmt,
@@ -37,7 +47,7 @@ null_ls.setup({
         '--no-keep-simple-function-one-line', '--no-break-after-operator', '--column-limit=100',
         '--break-after-table-lb', '--indent-width=2'
       }
-    }), formatting.isort, formatting.codespell.with({filetypes = {'markdown'}})
+    }), formatting.isort, formatting.codespell.with({ filetypes = { 'markdown' } })
   },
   on_attach = function(client, bufnr)
     custom_formatting(client, bufnr)
