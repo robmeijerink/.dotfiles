4817af348d30d0d010f77a462f7ce2de0562ce8d
Rob Meijerink <rmeijerink@avancecommunicatie.nl>
2022-09-25 00:44:42 +0200

    nvim tree number

--- .config/nvim/lua/robmeijerink/nvim-tree.lua
+++ .config/nvim/lua/robmeijerink/nvim-tree.lua
@@ -10,6 +10,8 @@ require("nvim-tree").setup({
   sort_by = "case_sensitive",
   view = {
     adaptive_size = true,
+    number = true,
+    relativenumber = true,
     mappings = {
       list = {
         { key = "u", action = "dir_up" },
