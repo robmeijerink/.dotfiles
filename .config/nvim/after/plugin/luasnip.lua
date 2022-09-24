490dc9be67b9755b81c14a1afd96b9ff1bbee180
Rob Meijerink <rmeijerink@avancecommunicatie.nl>
2022-09-24 12:30:25 +0200

    luasnip filetypes

--- .config/nvim/after/plugin/luasnip.lua
+++ .config/nvim/after/plugin/luasnip.lua
@@ -1,1 +1,6 @@
 require("luasnip.loaders.from_vscode").lazy_load()
+
+-- Extends Filetypes with frameworks etc.
+require('luasnip').filetype_extend("javascript", {"vue"})
+require('luasnip').filetype_extend("html", {"javascript"})
+require('luasnip').filetype_extend("blade", {"javascript"})
