b73276882b084a98602af0e696ebd12beb2298c1
Rob <rmeijerink@avancecommunicatie.nl>
2022-11-10 10:37:49 +0100

    fix wsl yank with autocmd

--- .config/nvim/lua/robmeijerink/settings.lua
+++ .config/nvim/lua/robmeijerink/settings.lua
@@ -103,8 +103,16 @@ vim.opt.wildignore = 'deps,.svn,CVS,.git,.hg,*.o,*.a,*.class,*.mo,*.la,*.so,*.ob
 if (string.find(vim.loop.os_uname().release, 'WSL')) then
 
     -- Fixes copy to clipboard in Windows WSL2
-    vim.opt.clipboard = 'unnamed'
+    -- vim.opt.clipboard = 'unnamed'
+  vim.opt.clipboard = "unnamedplus"
 
+  vim.api.nvim_create_autocmd('TextYankPost', {
+      group = vim.api.nvim_create_augroup('Yank', { clear = true }),
+
+      callback = function()
+          vim.fn.system('clip.exe', vim.fn.getreg('"'))
+      end,
+  })
 end
 
 -- Only show cursorline in the current window and in normal mode.
