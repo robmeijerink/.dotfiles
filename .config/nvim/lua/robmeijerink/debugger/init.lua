8b2cd4798b656cd783e4322c8f6646169a71f1a7
Rob Meijerink <rmeijerink@avancecommunicatie.nl>
2022-11-12 23:46:15 +0100

    debugger settings maps

--- .config/nvim/lua/robmeijerink/debugger/init.lua
+++ .config/nvim/lua/robmeijerink/debugger/init.lua
@@ -41,6 +41,7 @@ dap.listeners.before.event_exited["dapui_config"] = function()
 end
 
 require('robmeijerink.debugger.node')
+require('robmeijerink.debugger.php')
 
 -- require('dap').set_log_level('INFO')
 dap.defaults.fallback.terminal_win_cmd = '20split new'
@@ -48,24 +49,24 @@ vim.fn.sign_define('DapBreakpoint', {text='🟥', texthl='', linehl='', numhl=''
 vim.fn.sign_define('DapBreakpointRejected', {text='🟦', texthl='', linehl='', numhl=''})
 vim.fn.sign_define('DapStopped', {text='⭐️', texthl='', linehl='', numhl=''})
 
-nnoremap('<leader>b', function() require"dap".set_breakpoint() end)
-nnoremap('<leader>Dh', function() require"dap".toggle_breakpoint() end)
-nnoremap('<leader>DH', ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
+nnoremap('<leader>0', ':lua require"dap".set_breakpoint()<CR>')
+nnoremap('<leader>0h', function() require"dap".toggle_breakpoint() end)
+nnoremap('<leader>0H', ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
 nnoremap('<A-k>', function() require"dap".step_out() end)
 nnoremap("<A-l>", function() require"dap".step_into() end)
 nnoremap('<A-j>', function() require"dap".step_over() end)
 nnoremap('<A-h>', function() require"dap".continue() end)
-nnoremap('<leader>Dn', function() require"dap".run_to_cursor() end)
-nnoremap('<leader>Dc', function() require"dap".terminate() end)
-nnoremap('<leader>DR', function() require"dap".clear_breakpoints() end)
-nnoremap('<leader>De', function() require"dap".set_exception_breakpoints({"all"}) end)
-nnoremap('<leader>Da', function() require"debugHelper".attach() end)
-nnoremap('<leader>DA', function() require"debugHelper".attachToRemote() end)
-nnoremap('<leader>Di', function() require"dap.ui.widgets".hover() end)
-nnoremap('<leader>D?', function() local widgets=require"dap.ui.widgets";widgets.centered_float(widgets.scopes) end)
-nnoremap('<leader>Dk', ':lua require"dap".up()<CR>zz')
-nnoremap('<leader>Dj', ':lua require"dap".down()<CR>zz')
-nnoremap('<leader>Dr', ':lua require"dap".repl.toggle({}, "vsplit")<CR><C-w>l')
+nnoremap('<leader>0n', function() require"dap".run_to_cursor() end)
+nnoremap('<leader>0c', function() require"dap".terminate() end)
+nnoremap('<leader>0R', function() require"dap".clear_breakpoints() end)
+nnoremap('<leader>0e', function() require"dap".set_exception_breakpoints({"all"}) end)
+nnoremap('<leader>0a', function() require"debugHelper".attach() end)
+nnoremap('<leader>0A', function() require"debugHelper".attachToRemote() end)
+nnoremap('<leader>0i', function() require"dap.ui.widgets".hover() end)
+nnoremap('<leader>0?', function() local widgets=require"dap.ui.widgets";widgets.centered_float(widgets.scopes) end)
+nnoremap('<leader>0k', ':lua require"dap".up()<CR>zz')
+nnoremap('<leader>0j', ':lua require"dap".down()<CR>zz')
+nnoremap('<leader>0r', ':lua require"dap".repl.toggle({}, "vsplit")<CR><C-w>l')
 
 -- nvim-telescope/telescope-dap.nvim
 
