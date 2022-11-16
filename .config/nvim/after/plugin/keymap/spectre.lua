cc2fdbe3533eadf29f4ae6a014e29b4c5a125e66
Rob Meijerink <rmeijerink@avancecommunicatie.nl>
2022-11-17 00:58:15 +0100

    cr caps

--- .config/nvim/after/plugin/keymap/spectre.lua
+++ .config/nvim/after/plugin/keymap/spectre.lua
@@ -8,6 +8,6 @@ nnoremap("<leader>SS", "<cmd>lua require('spectre').open()<CR>")
 nnoremap("<leader>SW", "<cmd>lua require('spectre').open_visual({select_word=true})<CR>")
 vnoremap("<leader>SS", "<esc>:lua require('spectre').open_visual()<CR>")
 -- search in current file
-nnoremap("<leader>SP", "viw:lua require('spectre').open_file_search()<cr>")
+nnoremap("<leader>SP", "viw:lua require('spectre').open_file_search()<CR>")
 
 -- run command :Spectre
