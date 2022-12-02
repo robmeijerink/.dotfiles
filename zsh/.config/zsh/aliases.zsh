00b556d89fae6af9c7e091008d1f8ede17cf6cfa
Rob Meijerink <rmeijerink@avancecommunicatie.nl>
2022-12-02 11:53:56 +0100

    exa tree alias

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -9,7 +9,7 @@ alias dhalt="cd ~/devilbox && docker-compose down && cd -"
 alias dreload="cd ~/devilbox && docker-compose down && docker-compose up -d && cd -"
 alias dssh="cd ~/devilbox/ && ./shell.sh"
 
-# ls, the common ones I use a lot shortened for rapid fire usage
+# ls, the common ones I use exa for ls
 alias l='ls -lFh'     #size,show type,human readable
 alias la='ls -lAFh'   #long list,show almost all,show type,human readable
 alias lsa='ls -lah'
@@ -17,11 +17,9 @@ alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
 alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
 alias ll='ls -l'      #long list
 alias ldot='ls -ld .*'
-alias lS='ls -1FSsh'
-alias lart='ls -1Fcart'
-alias lrt='ls -1Fcrt'
-alias lsr='ls -lARFh' #Recursive list of files and directories
-alias lsn='ls -1'     #A column contains name of files and directories
+alias lst='ls -T'     #ls with tree
+alias llt='ls -lT'    #ls -l with tree
+alias lat='ls -laT'   #ls -la with tree
 
 # Colorize grep output (good for log files)
 alias grep='grep --color=auto'
