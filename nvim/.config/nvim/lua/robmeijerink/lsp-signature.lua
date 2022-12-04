0cbf7cbbb7ba1ab5490316110a82eea1cfa17eaa
Rob Meijerink <rmeijerink@avancecommunicatie.nl>
2022-12-04 21:17:22 +0100

    lsp signature: no panda prefix

--- nvim/.config/nvim/lua/robmeijerink/lsp-signature.lua
+++ nvim/.config/nvim/lua/robmeijerink/lsp-signature.lua
@@ -23,7 +23,9 @@ local cfg = {
   fix_pos = false, -- set to true, the floating window will not auto-close until finish all parameters
   hint_enable = true, -- virtual hint enable
   hint_scheme = "Comment",
-  use_lspsaga = false, -- set to true if you want to use lspsaga popup
+  -- hint_prefix = "🐼 ",  -- Panda for parameter, NOTE: for the terminal not support emoji, might crash
+  hint_prefix = '',
+  use_lspsaga = true, -- set to true if you want to use lspsaga popup
   hi_parameter = "LspSignatureActiveParameter", -- how your parameter will be highlight
   max_height = 12, -- max height of signature floating_window, if content is more than max_height, you can scroll down
   -- to view the hiding contents
