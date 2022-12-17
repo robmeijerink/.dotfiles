3975304a83574a9fa41a77c5fe0c86f84fca0a87
Rob Meijerink <rob.meijerink@gmail.com>
2022-12-17 02:05:20 +0100

    nvim: config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,7 @@
+-- disable netrw at the very start of your init.lua (strongly advised) -- nvim-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 require('robmeijerink.plugins')
 require('robmeijerink.settings')
 -- require('robmeijerink.debugger')
