e3521cf352fbeeeb2aa6e84d7e8edb90b96365cb
Rob Meijerink <rob.meijerink@gmail.com>
2023-02-13 11:37:23 +0100

    lsp: rename sumneko_lua -> lua_ls

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -16,7 +16,7 @@ lsp.ensure_installed({
   'rust_analyzer',
   'gopls',
   'tsserver',
-  'sumneko_lua',
+  'lua_ls',
   'volar',
   'tailwindcss',
   'emmet_ls',
@@ -75,29 +75,7 @@ local cmp_setup = {
         fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
       end
     end, {"i", "s"}),
-    ["<Down>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      -- elseif vim.fn["vsnip#available"]() == 1 then
-      --   feedkey("<Plug>(vsnip-expand-or-jump)", "")
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-      end
-    end, {"i", "s"}),
 
-    ["<Up>"] = cmp.mapping(function()
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      -- elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-      --   feedkey("<Plug>(vsnip-jump-prev)", "")
-      end
-    end, {"i", "s"}),
     ["<S-Tab>"] = cmp.mapping(function()
       if cmp.visible() then
         cmp.select_prev_item()
