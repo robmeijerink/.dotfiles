459d262d2a275bad981fbcfa48419d4ecbe543e6
Rob Meijerink <rob.meijerink@gmail.com>
2024-03-06 14:04:14 +0100

    nvim: init_local.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,10 @@
 require('robmeijerink.plugins')
 require('robmeijerink.settings')
 require('robmeijerink.debugger')
+
+-- Require local config file if the file exists
+local f = io.open('./init_local.lua', 'r')
+if f ~= nil then
+    io.close(f)
+    require('init_local')
+end
