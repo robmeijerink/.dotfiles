c639efa08e2c87f8ad46f658a60813d51b969315
Rob Meijerink <rob.meijerink@gmail.com>
2024-09-23 09:38:53 +0200

    update neoscroll mappings syntax

--- nvim/.config/nvim/lua/robmeijerink/plugins.lua
+++ nvim/.config/nvim/lua/robmeijerink/plugins.lua
@@ -334,11 +334,12 @@ require('lazy').setup({
     {
       'karb94/neoscroll.nvim',
       config = function()
-        require('neoscroll').setup()
-        require('neoscroll.config').set_mappings({
-          ['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '50' } },
-          ['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '50' } },
-        })
+        require('neoscroll').setup({
+                mappings = {
+                    ['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '50' } },
+                    ['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '50' } },
+                }
+            })
       end,
     },
 
