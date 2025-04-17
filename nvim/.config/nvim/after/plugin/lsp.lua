598b0c1470cf04d62b0c3f08256d98cdbd713ea4
Rob Meijerink <rob.meijerink@gmail.com>
2025-04-17 16:46:30 +0200

    Upgrade nvim 11

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -61,12 +61,28 @@ local lspkind = require('lspkind')
 local luasnip = require('luasnip')
 
 -- These signs show on the left, next to the line number
-local signs = {Error = " ", Warn = " ", Info = " ", Hint = "󰌶" }
+-- local signs = {Error = " ", Warn = " ", Info = " ", Hint = "󰌶" }
+--
+-- for type, icon in pairs(signs) do
+--   local hl = "DiagnosticSign" .. type
+--   vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = ""})
+-- end
 
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = ""})
-end
+-- Diagnostics
+vim.diagnostic.config({
+  virtual_text = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "",
+      [vim.diagnostic.severity.WARN]  = "",
+      [vim.diagnostic.severity.HINT]  = "",
+      [vim.diagnostic.severity.INFO]  = "",
+    }
+  }
+})
 
 lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
