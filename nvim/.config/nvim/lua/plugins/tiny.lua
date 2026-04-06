a67b33dd152a60bf4aafa3b9c56b2eda1cc531ea
Rob Meijerink <rob.meijerink@gmail.com>
2026-04-06 13:50:55 +0200

    cleanup old plugins after native replacements

--- nvim/.config/nvim/lua/plugins/tiny.lua
+++ nvim/.config/nvim/lua/plugins/tiny.lua
@@ -5,12 +5,10 @@ return {
     -- Tim Pope Essentials
     { "tpope/vim-surround" },
     { "tpope/vim-repeat" },
-    { "tpope/vim-unimpaired" },
-    { "tpope/vim-sleuth", lazy = true },
-    { "tpope/vim-eunuch", cmd = { "Rename", "SudoWrite", "Delete", "Chmod" } },
+    { "tpope/vim-eunuch",                       cmd = { "Rename", "SudoWrite", "Delete", "Chmod" } },
 
     -- Navigation & Editing
-    { "christoomey/vim-tmux-navigator", lazy = false },
+    { "christoomey/vim-tmux-navigator",         lazy = false },
     { "nelstrom/vim-visual-star-search" },
     { "jessarcher/vim-heritage" },
     { "moll/vim-bbye" },
@@ -32,14 +30,6 @@ return {
             vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
             vim.g.splitjoin_trailing_comma = 1
             vim.g.splitjoin_php_method_chain_full = 1
-        end
-    },
-
-    -- Smart Paste (Pasta)
-    {
-        'sickill/vim-pasta',
-        config = function()
-            vim.g.pasta_disabled_filetypes = { 'fugitive' }
         end
     },
 }
