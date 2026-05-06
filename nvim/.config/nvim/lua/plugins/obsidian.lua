808b2654ee402bc4988da11bec495796f13afd8a
Rob Meijerink <rob.meijerink@gmail.com>
2026-05-06 23:02:01 +0200

    add obsidian nvim plugin

---
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,66 @@
+-- =========================================================
+-- Plugin: Obsidian.nvim
+-- Focus: Markdown knowledge base and project documentation
+-- =========================================================
+return {
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    opts = {
+        workspaces = {
+            {
+                name = "global_vault",
+                path = "~/Dropbox/Obsidian",
+            },
+            {
+                name = "project_docs",
+                path = function()
+                    -- Falls back to the current working directory (useful for project /docs)
+                    return assert(vim.fn.getcwd())
+                end,
+            },
+        },
+
+        completion = {
+            nvim_cmp = true,
+            min_chars = 2,
+        },
+
+        note_id_func = function(title)
+            local suffix = ""
+            if title ~= nil then
+                suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+            else
+                suffix = tostring(os.time())
+            end
+            return tostring(os.date("%Y-%m-%d")) .. "-" .. suffix
+        end,
+
+        disable_frontmatter = false,
+
+        templates = {
+            folder = "templates",
+            date_format = "%Y-%m-%d",
+            time_format = "%H:%M",
+        },
+
+        mappings = {
+            ["gf"] = {
+                action = function()
+                    return require("obsidian").util.gf_passthrough()
+                end,
+                opts = { noremap = false, expr = true, buffer = true },
+            },
+            ["<leader>ch"] = {
+                action = function()
+                    return require("obsidian").util.toggle_checkbox()
+                end,
+                opts = { buffer = true },
+            },
+        },
+    },
+}
